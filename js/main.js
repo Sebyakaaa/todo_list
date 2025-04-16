a7d153ecad0811505890c7a5a766f9ba027a1b3e
Katsiaryna Sebiatsinskaya <katsiaryna.sebiatsinskaya@pandadoc.com>
2025-04-16 16:19:41 +0200

    add func for edit and new item

--- js/main.js
+++ js/main.js
@@ -8,17 +8,32 @@ const CHECKBOX_CTRL = 'checkbox__control';
 const TASK_TEXT = 'task-list__text';
 const DELETE_BTN = 'task-list__delete';
 
-// Функция для генерации случайного ID
-// function generateId() {
-//   return Math.floor(Math.random() * 1000000);
-// }
-
 function showLoader() {
   loader.hidden = false;
 }
 
 function hideLoader() {
   loader.hidden = true;
+}
+
+// загрузка задач с сервера
+async function loadTodosFromAPI() {
+  showLoader();
+
+  try {
+    const response = await fetch('https://dummyjson.com/todos');
+    const data = await response.json();
+    const todos = data.todos;
+
+    todos.forEach(todo => {
+      const taskItem = createTaskItem(todo.todo, todo.id, todo.completed);
+      taskList.insertAdjacentHTML('beforeend', taskItem); // добавление в конец
+    });
+  } catch (error) {
+    console.error('Loading error', error);
+  } finally {
+    hideLoader();
+  }
 }
 
 // Отображение списка задач
@@ -40,10 +55,10 @@ function createTaskItem(taskText, taskId, isCompleted = false) {
   `;
 }
 
-// Добавление задачи
-addTaskBtn.addEventListener('click', async () => {
+// Добавление новой задачи
+async function handleAddTaskClick(event) {
   const taskText = taskInput.value.trim();
-  if (!taskText) return;   // if (taskText !== '') {}
+  if (!taskText) return;
 
   showLoader();
   addTaskBtn.disabled = true;
@@ -62,10 +77,9 @@ addTaskBtn.addEventListener('click', async () => {
     });
 
     const newTodo = await response.json();
+    
+    renderTaskItem(newTodo);
 
-    // Рендерим новый todo
-    const taskItem = createTaskItem(newTodo.todo, newTodo.id, newTodo.completed);
-    taskList.insertAdjacentHTML('afterbegin', taskItem); // добавление в начало
     taskInput.value = '';
   } catch (error) {
     console.error('Can not add an item', error);
@@ -73,8 +87,15 @@ addTaskBtn.addEventListener('click', async () => {
     hideLoader();
     addTaskBtn.disabled = false;
   }
-});
+}
 
+//отображение новой задачи в списке
+async function renderTaskItem(newTodo) {
+    const taskItem = createTaskItem(newTodo.todo, newTodo.id, newTodo.completed);
+    taskList.insertAdjacentHTML('afterbegin', taskItem);
+}
+
+// Комплит задачи
 async function handleCheckboxClick(event) {
   if (event.target.classList.contains(CHECKBOX_CTRL)) {
     const checkbox = event.target;
@@ -106,6 +127,36 @@ async function handleCheckboxClick(event) {
   }
 }
 
+// Апдейт текста задачи
+async function handleTaskEditBlur(event) {
+  if (event.target.classList.contains(TASK_TEXT)) {
+    const taskItem = event.target.closest(`.${TASK_ITEM}`);
+    const taskId = taskItem.dataset.id;
+    const newText = event.target.textContent.trim();
+
+    if (!taskId) return;
+
+    try {
+      const response = await fetch(`https://dummyjson.com/todos/${taskId}`, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ todo: newText })
+      });
+
+      if (!response.ok) {
+        throw new Error('Failed to update task text');
+      }
+
+    } catch (error) {
+      alert('Failed to update task text');
+      console.error(error);
+    }
+  }
+}
+
+// удаление задачи
 async function handleDeleteClick(event) {
   if (event.target.classList.contains(DELETE_BTN)) { 
     const taskItem = event.target.closest(`.${TASK_ITEM}`);
@@ -136,60 +187,12 @@ async function handleDeleteClick(event) {
   }
 }
 
-taskList.addEventListener('click', handleCheckboxClick);
-taskList.addEventListener('click', handleDeleteClick);
-
-// Апдейт текста задачи
-// Event Capturing
-taskList.addEventListener('blur', async (event) => {
-  if (event.target.classList.contains(TASK_TEXT)) {
-    const taskItem = event.target.closest(`.${TASK_ITEM}`);
-    const taskId = taskItem.dataset.id;
-    const newText = event.target.textContent.trim(); // textContent возвращает текст внутри элемента, который отредактировал юзер
-
-    if (!taskId) return;
-
-    try {
-      const response = await fetch(`https://dummyjson.com/todos/${taskId}`, {
-        method: 'PUT',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ todo: newText })
-      });
-
-      if (!response.ok) {
-        throw new Error('Failed to update task text');
-      }
-
-    } catch (error) {
-      alert('Failed to update task text');
-      console.error(error);
-    }
-  }
-}, true); // true (включает режим захвата) чтобы обработчик увидел blur
-
-// Функция для загрузки задач с сервера
-async function loadTodosFromAPI() {
-  showLoader();
-
-  try {
-    const response = await fetch('https://dummyjson.com/todos');
-    const data = await response.json();
-    const todos = data.todos;
-
-    todos.forEach(todo => {
-      const taskItem = createTaskItem(todo.todo, todo.id, todo.completed);
-      taskList.insertAdjacentHTML('beforeend', taskItem); // добавление в конец
-    });
-  } catch (error) {
-    console.error('Loading error', error);
-  } finally {
-    hideLoader();
-  }
-}
-
 // Вызов функции при загрузке страницы
 document.addEventListener('DOMContentLoaded', () => {
   loadTodosFromAPI();
 });
+
+addTaskBtn.addEventListener('click', handleAddTaskClick);
+taskList.addEventListener('click', handleCheckboxClick);
+taskList.addEventListener('blur', handleTaskEditBlur, true);
+taskList.addEventListener('click', handleDeleteClick);
